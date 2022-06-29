6b610cdb298f75470172e57e2ecf11a5e43b57d8
emelymack <emelymack@gmail.com>
2022-06-28 21:32:51 -0300

    Added fixes on Service annotations

--- src/main/java/com/dh/clinicaOdontologicaProject/service/AppointmentService.java
+++ src/main/java/com/dh/clinicaOdontologicaProject/service/AppointmentService.java
@@ -3,10 +3,12 @@ package com.dh.clinicaOdontologicaProject.service;
 import com.dh.clinicaOdontologicaProject.entity.Appointment;
 import com.dh.clinicaOdontologicaProject.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class AppointmentService implements IAppointmentService{
 
     @Autowired
--- src/main/java/com/dh/clinicaOdontologicaProject/service/DentistService.java
+++ src/main/java/com/dh/clinicaOdontologicaProject/service/DentistService.java
@@ -4,10 +4,12 @@ import com.dh.clinicaOdontologicaProject.entity.Dentist;
 import com.dh.clinicaOdontologicaProject.repository.DentistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class DentistService implements IDentistService{
 
     @Autowired
