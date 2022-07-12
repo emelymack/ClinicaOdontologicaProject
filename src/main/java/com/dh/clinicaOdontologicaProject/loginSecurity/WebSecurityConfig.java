be10ba7e0e9e852545e686068d92acadf88782b5
emelymack <emelymack@gmail.com>
2022-07-12 20:29:47 -0300

    Added changes in style

--- src/main/java/com/dh/clinicaOdontologicaProject/loginSecurity/WebSecurityConfig.java
+++ src/main/java/com/dh/clinicaOdontologicaProject/loginSecurity/WebSecurityConfig.java
@@ -27,8 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/get_all_dentists.html", "/get_all_patients.html", "/get_all_appointments.html").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/post_dentist.html", "/post_patient.html", "/post_appointment.html").hasRole("ADMIN")
+                .antMatchers("/", "/post_appointment.html","/get_all_appointments.html").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/post_dentist.html", "/get_all_dentists.html","/post_patient.html", "/get_all_patients.html").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and().formLogin()
                 .and().logout();
